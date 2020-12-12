2c6a750e68618046ff210b9fdaafb7141b581a6b
0712023 <0712023@hanmail.net>
2020-12-13 05:30:31 +0900

    ver 1.3 / mqtt added

--- voice.py
+++ voice.py
@@ -1,32 +1,28 @@
 import snowboydecoder
 import sys
 import signal
+import paho.mqtt.client as mqtt
 import serial
 import time
 
 #arduino serial function
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
 
-#snowboy function
-interrupted = False
+#create mqtt client object(voice detector ip address)
+mqtt = mqtt.Client()
+mqtt.connect("localhost", 1883)
 
+interrupted = False
 
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
-
 def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) != 3:
-    print("Error: need to specify 2 model names")
-    print("Usage: python demo.py 1st.model 2nd.model")
-    sys.exit(-1)
-
 models = sys.argv[1:]
-print(models)
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
@@ -34,19 +30,19 @@ signal.signal(signal.SIGINT, signal_handler)
 sensitivity = [0.5]
 detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
 
-def callback1(): #initiate detected
+def callback1():
+    mqtt.publish("/control", "111")
     ser.write(str.encode('1'))
 
-def callback2(): #stop detected
+def callback2():
+    mqtt.publish("/control", "000")
     ser.write(str.encode('0'))
 
+mqtt.loop(2)                      #timeout 2 sec
 
 callbacks = [callback1, callback2]
 
 # main loop
-# make sure you have the same numbers of callbacks and models
-detector.start(detected_callback=callbacks,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.03)
+detector.start(detected_callback=callbacks, interrupt_check=interrupt_callback, sleep_time=0.03)
 
 detector.terminate()
