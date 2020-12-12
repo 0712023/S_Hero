c5366f8610e2566cacd9062b6710628e5b8b5532
0712023 <0712023@hanmail.net>
2020-12-13 05:17:12 +0900

    ver 1.1

--- servo control with GPIO and mqtt.py
+++ servo control with GPIO and mqtt.py
@@ -13,6 +13,7 @@ def on_message(client, userdata, msg):
     global signal
     signal = msg.payload
 
+
 client = mqtt.Client()                      #create client object
 client.on_connect = on_connect      #set callback
 client.on_message = on_message #set callback
@@ -21,7 +22,7 @@ client.connect("192.168.0.3", 1883)
 GPIO.setmode(GPIO.BOARD) #BCM or board
 GPIO.setup(11, GPIO.IN)
 
-ser = serial.Serial('/dev/ttyUSB1', 9600) #check USB list
+ser = serial.Serial('/dev/ttyUSB0', 9600) #check USB list
 
 data_saved = []
 signal = 0
@@ -32,22 +33,18 @@ while True :
     if signal != None:
         data_saved.append(signal)
         signal = None
-    if m == 0:
-        if GPIO.input(11) == 0:
-            if data_saved[n] == None:
-                n += -1
-                print('no data')
-            else:
-                print(data_saved[n])
-                if data_saved[n] == b'33':#product is defective
-                    ser.write('2'.encode())
-                    print("unsatisified")
-                elif data_saved[n] == b'22':#product is adequate
-                    print("satisfied")
-            n += 1
-            m = 1
-            time.sleep(0.25)
-    if GPIO.input(11) == 1: #non-detected
+    if GPIO.input(11) == 1:
         m = 0
+    elif GPIO.input(11) == 0:
+        m += 1
+        print(m)
+    if m == 1:
+        print(data_saved)
+        if data_saved[n] == b'33':#product is defective
+            ser.write('2'.encode())
+            print("unsatisified")
+        elif data_saved[n] == b'22':#product is adequate
+            print("satisfied")
+        n += 1
         time.sleep(0.25)
     client.loop_stop()
