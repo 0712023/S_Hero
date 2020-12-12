1564df73e3b4f7378f7891de512c67aa52ac03d5
0712023 <0712023@hanmail.net>
2020-12-13 05:14:30 +0900

    ver 1.1

--- video stream.py
+++ video stream.py
@@ -1,32 +1,22 @@
-# import the necessary packages
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2 as cv
 import numpy as np
 
-# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
-
-# allow the camera to warmup
 time.sleep(0.1)
-
-# capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
     img = frame.array
-    image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    cv.imshow("my room", image)
+    rows, cols = img.shape[:2]
+    rotate = cv.getRotationMatrix2D((cols/2, rows/2),180, 1)
+    rotateimage = cv.warpAffine(img, rotate,(cols, rows))
+    cv.imshow("my room", rotateimage)
     key = cv.waitKey(1)
-
-	# clear the stream in preparation for the next frame
     rawCapture.truncate(0)
-
-	# if the `esc` key was pressed, break from the loop
     if key == 27:
         break
 cv.destroyAllWindows()
