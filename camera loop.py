1a3cb55f9c1cfd73ce27b00d9d8f661d5bc5b240
0712023 <0712023@hanmail.net>
2020-12-13 05:46:42 +0900

    ver 1.5 / GPIO added

--- camera loop.py
+++ camera loop.py
@@ -1,12 +1,15 @@
 import picamera
 import numpy as np
 import cv2 as cv
-from pyfirmata import Arduino, util
 import time
 import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.IN)
 
 #callback which is called when client get CONNACK response from server
-def connect_connect(client, userdata, flags, rc):
+def control_connect(client, userdata, flags, rc):
     print("Connected with voice ip_/start" + str(rc))
     client.subscribe("/control")
 
@@ -28,7 +31,7 @@ def control_message(client, userdata, msg):
 controlsub = mqtt.Client()
 
 #connect to MQTT server
-controlsub.connect("203.252.47.59", 1883)             #voice detector ip address
+controlsub.connect("192.168.0.13", 1883)             #voice detector ip address
 controlsub.on_connect = control_connect
 controlsub.on_message = control_message
 
@@ -39,12 +42,6 @@ controlpub = mqtt.Client()                         #send conveyor state to serve
 controlpub.connect("localhost",1883)
 
 #Arduino serial communication
-board = Arduino('/dev/ttyUSB0')
-pin9_sensor = board.get_pin('d:9:i')
-it = util.Iterator(board)
-it.start()
-pin9_sensor.enable_reporting
-pin_code = pin9_sensor
 
 #make first loop to initiate
 sign = b'111'
@@ -55,7 +52,7 @@ if sign == '111':
 
 n = 0 #int that make function taken only one time
 p = 0 #product number counter
-
+m = 0
 #main loop
 while True:
     controlpub.loop_start()
@@ -63,45 +60,49 @@ while True:
     servopub.loop_start()
     if sign == b'111':
         controlpub.publish("/conveyorstate", "111111")
-        if pin_code.read() == False:
-            n += 1
-            if n == 1:
-                p += 1
-                #function start
-                print('start circle detection')
-                with picamera.PiCamera() as camera:
-                    camera.resolution = (320, 240)
-                    camera.framerate = 24
-                    image = np.empty((240, 320, 3), dtype=np.uint8)
-                    camera.capture(image, 'bgr')
-                    #start circledetection
-                    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-                    circles = cv.HoughCircles(image_gray, cv.HOUGH_GRADIENT, 1, 20, np.array([]), 50, 60, 20, 30)
-                    if circles is not None:
-                        if len(circles.shape) == 3:
-                            a, b, c = circles.shape
-                            group = "1"#adequate
-                            servopub.publish("/servo", "22")          #same string size, 22222222 means product is adquate
-                        else:
-                            b = 0
-                            group = "0"#defective
-                            servopub.publish("/servo", "33")          #same string size, 33333333 means product is defective
-                        for i in range(b):
-                            cv.circle(image, (circles[0][i][0], circles[0][i][1]), circles[0][i][2], (0, 0, 255), 3, cv.LINE_AA)
-                #save result at log.txt
-                print('saving result')
-                p1 = str(p)
-                log = open('/home/pi/coding/product_result/results.txt', "a")
-                log.write('product' + p1 + ' is ' + group + '\n')
-                log.close()
-                #save image
-                print('saving product picture')
-                name = p1 + '.jpg'
-                cv.imwrite(name, '/home/pi/coding/product_result', image)
-                cv.destroyAllWindows()
-                #function end
+        if m == 0:
+            if GPIO.input(11) == 0:
+                n += 1
+                if n == 1:
+                    p += 1
+                    #function start
+                    print('start circle detection')
+                    with picamera.PiCamera() as camera:
+                        camera.resolution = (320, 240)
+                        camera.framerate = 24
+                        image = np.empty((240, 320, 3), dtype=np.uint8)
+                        camera.capture(image, 'bgr')
+                        #start circledetection
+                        image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+                        circles = cv.HoughCircles(image_gray, cv.HOUGH_GRADIENT, 1, 20, np.array([]), 50, 60, 20, 30)
+                        if circles is not None:
+                            if len(circles.shape) == 3:
+                                a, b, c = circles.shape
+                                group = "1"#adequate
+                                servopub.publish("/servo", "22")          #same string size, 22222222 means product is adquate
+                            else:
+                                b = 0
+                                group = "0"#defective
+                                servopub.publish("/servo", "33")          #same string size, 33333333 means product is defective
+                            for i in range(b):
+                                cv.circle(image, (circles[0][i][0], circles[0][i][1]), circles[0][i][2], (0, 0, 255), 3, cv.LINE_AA)
+                    #save result at log.txt
+                    print('saving result')
+                    p1 = str(p)
+                    log = open('/home/pi/coding/product_result/results.txt', "a")
+                    log.write('product' + p1 + ' is ' + group + '\n')
+                    log.close()
+                    #save image
+                    print('saving product picture')
+                    name = p1 + '.jpg'
+                    cv.imwrite(name, image)
+                    cv.destroyAllWindows()
+                    #function end
         else:
             n = 0
+        if GPIO.input(11) == 1:
+            m = 0
+            time.sleep(0.25)
     controlpub.publish("/conveyorstate", "000000")
     time.sleep(1)
     controlpub.loop_stop()
